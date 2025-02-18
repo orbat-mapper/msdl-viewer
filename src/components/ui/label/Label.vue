b5080d3fbe8c1434e1a432fe508b7a72c7ccbc1e
KMF <kjellmf@gmail.com>
2025-02-18 19:15:56 +0100

    refactor: rename main view

--- src/components/ui/label/Label.vue
+++ src/components/ui/label/Label.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import type { LabelProps } from 'radix-vue'
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
-import { Label } from 'radix-vue'
-import { computed } from 'vue'
+import type { LabelProps } from "radix-vue";
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+import { Label } from "radix-vue";
+import { computed } from "vue";
 
-const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<LabelProps & { class?: HTMLAttributes["class"] }>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+  const { class: _, ...delegated } = props;
 
-  return delegated
-})
+  return delegated;
+});
 </script>
 
 <template>
